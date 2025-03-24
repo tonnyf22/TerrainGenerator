e385c75413113af3613a5262ac74694711899cd7
Tonny <dimb0r22@gmail.com>
2025-03-24 22:38:22 +0200

    chore(monob): back to defaults

--- Assets/Scripts/TerrainGenerator/Components/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGenerator/Components/TerrainGeneration.cs
@@ -2,29 +2,29 @@ using TerrainGenerator.Components.Settings.Bioms;
 using TerrainGenerator.Components.Settings.Chunks;
 using NaughtyAttributes;
 using UnityEngine;
+using System.Collections.Generic;
+using TerrainGenerator.Generation.Surface.Structure;
 
 namespace TerrainGenerator.Components
 {
     [DisallowMultipleComponent]
     public class TerrainGeneration : MonoBehaviour
     {
-        [Header("General")]
-        [HorizontalLine()]
         public string seed;
         public Transform generationCenter;
-
-        [Space(20)]
-
-        [Header("Chunks settings")]
-        [HorizontalLine()]
-        [Expandable]
         public ChunksSettings chunksSettings;
-
-        [Space(20)]
-
-        [Header("Bioms system settings")]
-        [HorizontalLine()]
-        [Expandable]
         public BiomsSystemSettings biomsSystemSettings;
+
+        public Dictionary<ChunkCoordinates, Chunk> chunks = new Dictionary<ChunkCoordinates, Chunk>();
+
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
     }
 }
