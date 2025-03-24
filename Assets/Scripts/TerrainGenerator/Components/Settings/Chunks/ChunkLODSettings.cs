b47bfb7242e5796a500c9ccc26c32e574b421853
Tonny <dimb0r22@gmail.com>
2025-03-24 22:40:10 +0200

    feat(chunk lod settings): add new fields, remove view attributes

--- Assets/Scripts/TerrainGenerator/Components/Settings/Chunks/ChunkLODSettings.cs
+++ Assets/Scripts/TerrainGenerator/Components/Settings/Chunks/ChunkLODSettings.cs
@@ -5,12 +5,14 @@ namespace TerrainGenerator.Components.Settings.Chunks
     [CreateAssetMenu(fileName = "ChunkLODSettings", menuName = "Terrain Generator/Chunks/Chunk LOD Settings")]
     public class ChunkLODSettings : ScriptableObject
     {
-        public MeshFillType meshFillType;
-        [Range(2, 512)]
-        public int meshResolution = 2;
-        [Min(0)]
         public float maxDistance;
-        [Range(2, 64)]
-        public int waterGridCoveringResolution = 2;
+        public MeshFillType meshFillType;
+        public int meshResolution = 2;
+        public bool isApplyCollision;
+        public bool isApplyWaterCovering = true;
+        public int waterCoveringMeshResolution = 2;
+        public bool isApplyScattering = true;
+        public bool isApplyScatteringSparcing;
+        public int scatteringSparceLevel = 1;
     }
 }
