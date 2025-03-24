7e5b5978ab8bea04ca7a39be60749842063fdb1d
Tonny <dimb0r22@gmail.com>
2025-03-24 22:41:34 +0200

    chore(atributes): remove view attributes

--- Assets/Scripts/TerrainGenerator/Components/Settings/Chunks/ChunksSettings.cs
+++ Assets/Scripts/TerrainGenerator/Components/Settings/Chunks/ChunksSettings.cs
@@ -6,9 +6,7 @@ namespace TerrainGenerator.Components.Settings.Chunks
     [CreateAssetMenu(fileName = "ChunksSettings", menuName = "Terrain Generator/Chunks/Chunks Settings")]
     public class ChunksSettings : ScriptableObject
     {
-        [Min(0)]
         public float chunkSize;
-        [Expandable]
         public ChunkLODSettings[] chunkLODSettings;
         public float chunkPreloadMaxDistance;
     }
