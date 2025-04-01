614337e18fc9fd8847d40eeb458614cd1f4bf104
Tonny <dimb0r22@gmail.com>
2025-04-01 23:04:06 +0300

    feat(biome settings): store biome graph as field

--- Assets/Scripts/TerrainGenerator/Components/Settings/Biomes/BiomeSettings.cs
+++ Assets/Scripts/TerrainGenerator/Components/Settings/Biomes/BiomeSettings.cs
@@ -1,3 +1,4 @@
+using TerrainGenerator.Components.Settings.Biomes.BiomeNodeGraph;
 using UnityEngine;
 
 namespace TerrainGenerator.Components.Settings.Biomes
@@ -5,6 +6,6 @@ namespace TerrainGenerator.Components.Settings.Biomes
     [CreateAssetMenu(fileName = "BiomeSettings", menuName = "Terrain Generator/Biomes/Biome Settings")]
     public class BiomeSettings : ScriptableObject
     {
-
+        public BiomeGraph biomeNodeGraph;
     }
 }
