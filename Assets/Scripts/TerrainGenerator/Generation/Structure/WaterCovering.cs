d00133cab178df69d33f50969c02b3668c7a5124
Tonny <dimb0r22@gmail.com>
2025-04-07 16:59:15 +0300

    feat(water coverinng): changes after debug session
    rename set mesh method to add clarity
    add collision method
    check for empty triangles array in mesh
    comment code about which is uncertain

--- Assets/Scripts/TerrainGenerator/Generation/Structure/WaterCovering.cs
+++ Assets/Scripts/TerrainGenerator/Generation/Structure/WaterCovering.cs
@@ -1,43 +1,73 @@
+using TerrainGenerator.Components.Settings.Chunks;
 using UnityEngine;
 
 namespace TerrainGenerator.Generation.Structure
 {
     public class WaterCovering
     {
-        public static WaterCovering CreateWaterCovering(bool isApplyWaterCovering, int waterCoveringMeshResolution, GameObject parentGameObject)
+        public static WaterCovering CreateWaterCovering(/* bool isApplyWaterCovering, */ MeshFillType waterCoveringMeshFillType, int waterCoveringMeshResolution, GameObject parentGameObject)
         {
             return new WaterCovering(
-                isApplyWaterCovering,
+                // isApplyWaterCovering,
+                waterCoveringMeshFillType,
                 waterCoveringMeshResolution,
                 parentGameObject
             );
         }
 
-        public readonly bool isApplyWaterCovering;
+        // public readonly bool isApplyWaterCovering;
+        public readonly MeshFillType waterCoveringMeshFillType;
         public readonly int waterCoveringMeshResolution;
         public readonly GameObject waterCoveringGameObject;
 
-        public WaterCovering(bool isApplyWaterCovering, int waterCoveringMeshResolution, GameObject parentGameObject)
+        public WaterCovering(/* bool isApplyWaterCovering, */ MeshFillType waterCoveringMeshFillType, int waterCoveringMeshResolution, GameObject parentGameObject)
         {
-            this.isApplyWaterCovering = isApplyWaterCovering;
+            // this.isApplyWaterCovering = isApplyWaterCovering;
+            this.waterCoveringMeshFillType = waterCoveringMeshFillType;
             this.waterCoveringMeshResolution = waterCoveringMeshResolution;
             waterCoveringGameObject = new GameObject("WaterCovering");
             waterCoveringGameObject.transform.parent = parentGameObject.transform;
+            SetWaterCoveringGameObjectCoordinates();
         }
 
-        public void ApplyWaterCoveringMesh(Mesh mesh)
+        private void SetWaterCoveringGameObjectCoordinates()
         {
-            if (waterCoveringGameObject.TryGetComponent<MeshFilter>(out var meshFilter))
+            waterCoveringGameObject.transform.localPosition = Vector3.zero;
+        }
+
+        public void ApplyWaterCoveringMesh(Mesh mesh, Material material)
+        {
+            if (mesh.triangles.Length == 0)
+            {
+                return;
+            }
+
+            if (!waterCoveringGameObject.TryGetComponent(out MeshFilter meshFilter))
             {
                 meshFilter = waterCoveringGameObject.AddComponent<MeshFilter>();
             }
-            if (waterCoveringGameObject.TryGetComponent<MeshRenderer>(out var meshRenderer))
+            if (!waterCoveringGameObject.TryGetComponent(out MeshRenderer meshRenderer))
             {
                 meshRenderer = waterCoveringGameObject.AddComponent<MeshRenderer>();
             }
 
             meshFilter.mesh = mesh;
-            // meshRenderer.material = ...;
+            meshRenderer.material = material;
+        }
+
+        public void ApplyWaterCoveringCollision(Mesh mesh)
+        {
+            if (mesh.triangles.Length == 0)
+            {
+                return;
+            }
+
+            if (!waterCoveringGameObject.TryGetComponent(out MeshCollider meshCollider))
+            {
+                meshCollider = waterCoveringGameObject.AddComponent<MeshCollider>();
+            }
+
+            meshCollider.sharedMesh = mesh;
         }
     }
 }
