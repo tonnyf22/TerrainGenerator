6e1b09472eb8d7fce925e12fcab1733514f4b4e4
Tonny <dimb0r22@gmail.com>
2025-04-27 00:58:23 +0300

    feat(object scattering): integrate objects pools

--- Assets/Scripts/TerrainGenerator/Generation/Structure/ObjectsScattering.cs
+++ Assets/Scripts/TerrainGenerator/Generation/Structure/ObjectsScattering.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using TerrainGenerator.Generation.Scattering;
 using UnityEngine;
 
 namespace TerrainGenerator.Generation.Structure
@@ -16,6 +18,10 @@ namespace TerrainGenerator.Generation.Structure
         public readonly bool isApplyScatteringSparseLevel;
         public readonly int scatteringSparseLevel;
         public readonly GameObject objectsScatteringGameObject;
+        public ScatteringObjectsPool scatteringObjectsPool { get; private set; }
+
+        private Dictionary<BiomeScattering, List<ScatteringObjectParameters>> biomeScatteringObjectsParameters;
+        private Dictionary<BiomeScattering, List<GameObject>> biomeScatteringObjects;
 
         public ObjectsScattering(bool isApplyScatteringSparseLevel, int scatteringSparseLevel, GameObject parentGameObject)
         {
@@ -31,12 +37,90 @@ namespace TerrainGenerator.Generation.Structure
             objectsScatteringGameObject.transform.localPosition = Vector3.zero;
         }
 
-        public void ApplyScatteringGameObjects(List<GameObject> scatteringGameObjects)
+        public void SaveBiomeScatteringObjectsParameters(Dictionary<BiomeScattering, List<ScatteringObjectParameters>> biomeScatteringObjectsParameters)
         {
-            foreach (var item in scatteringGameObjects)
+            if (this.biomeScatteringObjectsParameters != null)
             {
-                item.transform.parent = objectsScatteringGameObject.transform;
+                throw new ArgumentException($"Biome scattering objects parameters already exists for this object scattering.");
             }
+            else
+            {
+                this.biomeScatteringObjectsParameters = biomeScatteringObjectsParameters;
+
+                biomeScatteringObjects = new Dictionary<BiomeScattering, List<GameObject>>(biomeScatteringObjectsParameters.Count);
+            }
+        }
+
+        public void ApplyScatteringObjectsPool(ScatteringObjectsPool scatteringObjectsPool)
+        {
+            if (this.scatteringObjectsPool != null)
+            {
+                throw new ArgumentException($"Scattering objects pool already exists for this objects scattering.");
+            }
+            else
+            {
+                this.scatteringObjectsPool = scatteringObjectsPool;
+            }
+        }
+
+        public void RemoveScatteringObjectsPool()
+        {
+            if (this.scatteringObjectsPool == null)
+            {
+                throw new ArgumentException($"Scattering objects pool does not exist for this objects scattering.");
+            }
+            else
+            {
+                this.scatteringObjectsPool = null;
+            }
+        }
+
+        public void SetupScattering()
+        {
+            foreach (var pair in biomeScatteringObjectsParameters)
+            {
+                List<GameObject> listGameObjects = new List<GameObject>();
+
+                for (int indexObject = 0; indexObject < pair.Value.Count; indexObject++)
+                {
+                    // get object
+                    // setup objects' transform, parent
+                    // add object to list
+
+                    GameObject gameObject = scatteringObjectsPool.GetScatteringObject(pair.Key);
+
+                    gameObject.transform.parent = objectsScatteringGameObject.transform;
+
+                    gameObject.transform.position = pair.Value[indexObject].position;
+                    gameObject.transform.localScale = pair.Value[indexObject].scale;
+                    gameObject.transform.rotation = pair.Value[indexObject].rotation;
+
+                    listGameObjects.Add(gameObject);
+                }
+                
+                biomeScatteringObjects.Add(pair.Key, listGameObjects);
+            }
+        }
+
+        public void ReleaseScattering()
+        {
+            // go through biomeScatteringObjects
+            // unparent each object
+            // put object back to pool
+
+            foreach (var pair in biomeScatteringObjects)
+            {
+                for (int indexObject = 0; indexObject < pair.Value.Count; indexObject++)
+                {
+                    GameObject gameObject = pair.Value[indexObject];
+
+                    gameObject.transform.parent = null;
+
+                    scatteringObjectsPool.PutScatteringObject(pair.Key, gameObject);
+                }
+            }
+
+            biomeScatteringObjects.Clear();
         }
     }
 }
