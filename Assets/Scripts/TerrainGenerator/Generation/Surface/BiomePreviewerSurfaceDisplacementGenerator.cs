5aecc66f44e0487c3e036c2ae22383350fd19f5f
Tonny <dimb0r22@gmail.com>
2025-05-09 13:22:11 +0300

    feat(generators): create specific generators for biome previewer (by regular generators templates)

---
+++ Assets/Scripts/TerrainGenerator/Generation/Surface/BiomePreviewerSurfaceDisplacementGenerator.cs
@@ -0,0 +1,65 @@
+using TerrainGenerator.Generation.Biome;
+using TerrainGenerator.Generation.Structure;
+using UnityEngine;
+
+namespace TerrainGenerator.Generation.Surface
+{
+    public class BiomePreviewerSurfaceDisplacementGenerator : ISurfaceDisplacementGenerator
+    {
+        public static BiomePreviewerSurfaceDisplacementGenerator CreateDisplacementGenerator(Chunk chunk, BiomeGraphInterpreter biomeGraphInterpreter)
+        {
+            return new BiomePreviewerSurfaceDisplacementGenerator(
+                chunk,
+                biomeGraphInterpreter
+            );
+        }
+
+        public readonly Chunk chunk;
+        public readonly BiomeGraphInterpreter biomeGraphInterpreter;
+
+        public BiomePreviewerSurfaceDisplacementGenerator(Chunk chunk, BiomeGraphInterpreter biomeGraphInterpreter)
+        {
+            this.chunk = chunk;
+            this.biomeGraphInterpreter = biomeGraphInterpreter;
+        }
+
+        public void ApplyDisplacementToMesh(Mesh mesh)
+        {
+            Vector3[] meshVertices = mesh.vertices;
+            Vector3[] displacedVertices = new Vector3[meshVertices.Length];
+
+            for (int vertexIndex = 0; vertexIndex < meshVertices.Length; vertexIndex++)
+            {
+                Vector3 vertexLocal = meshVertices[vertexIndex];
+                Vector3 vertexGlobal = CalculateGlobalCoordinatesOfVertexLocal(vertexLocal);
+
+                float height = GetHeightFromBiomeInVertexGlobal(vertexGlobal);
+
+                displacedVertices[vertexIndex] = new Vector3(
+                    vertexLocal.x,
+                    height,
+                    vertexLocal.z
+                );
+            }
+
+            mesh.SetVertices(displacedVertices);
+        }
+
+        private Vector3 CalculateGlobalCoordinatesOfVertexLocal(Vector3 vertexLocal)
+        {
+            return Chunk.LocatePointInChunkAsPoint(
+                vertexLocal.x,
+                vertexLocal.z,
+                chunk.chunkCoordinates,
+                chunk.chunkSize);
+        }
+
+        private float GetHeightFromBiomeInVertexGlobal(Vector3 vertexGlobal)
+        {
+            return biomeGraphInterpreter.GetBiomeHeight(
+                0,
+                vertexGlobal.x,
+                vertexGlobal.z);
+        }
+    }
+}
